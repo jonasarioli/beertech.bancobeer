e5ba0f3f738d93ac528ebb068fd7e13089e6093b
Jonas Arioli <jonas.arioli82@gmail.com>
2020-10-26 10:36:59 -0300

    Validação do token no reward

--- banco-beer-product/src/main/java/com/beertech/product/controller/ProductController.java
+++ banco-beer-product/src/main/java/com/beertech/product/controller/ProductController.java
@@ -1,8 +1,7 @@
 package com.beertech.product.controller;
 
-import com.beertech.product.controller.dto.ContaDto;
 import com.beertech.product.controller.form.ProductForm;
-import com.beertech.product.model.Conta;
+import com.beertech.product.exception.ProductRewardException;
 import com.beertech.product.model.Product;
 import com.beertech.product.service.ProductService;
 import io.swagger.annotations.ApiImplicitParam;
@@ -16,27 +15,16 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.client.RestClientException;
-import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import org.springframework.web.util.UriComponentsBuilder;
 import springfox.documentation.annotations.ApiIgnore;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.lang.reflect.Field;
-import java.math.BigDecimal;
 import java.net.URI;
-import java.security.Principal;
-import java.util.LinkedHashMap;
 import java.util.Optional;
 
 @RestController
@@ -122,21 +110,14 @@ public class ProductController {
     }
 
     @PostMapping("/reward/{id}")
-    public ResponseEntity rewardProoduct(@PathVariable Long id, @ApiIgnore Principal principal) {
-        Optional<Product> productById = productService.findProductById(id);
-        if(productById.isPresent()) {
-            String token = "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiIyIiwiUGVyZmlsIjoiUk9MRV9VU0VSIiwiU2FsZG8iOjEwMC4wMCwiTm9tZSI6IlVTRVIiLCJIYXNoIjoiNWM0N2QyNWUzZTk4YjUzYWJjNTMyYTNlNzcyMzAzN2YiLCJleHAiOjE2MDM3NTI4MDksImlhdCI6MTYwMzY2NjQwOX0.1QjFFj5lxdD2DMO8Dfhkkir0Uari6Ire_WkozmBPuMM";
-            ContaDto conta = null;
-            try {
-                conta = validation(token);
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-            productService.rewardProduct(productById.get(), conta.getHash(), conta.getSaldo());
-            return ResponseEntity.accepted().build();
-        } else {
-            return ResponseEntity.notFound().build();
+    public ResponseEntity rewardProoduct(@RequestHeader (name="Authorization") String token, @PathVariable Long id) {
+        try {
+            productService.rewardProduct(id, token);
+        } catch (ProductRewardException ex) {
+            System.out.println(ex.getMessage());
+            return ResponseEntity.status(ex.getHttpStatusCode()).build();
         }
+        return ResponseEntity.accepted().build();
     }
 
     @ApiIgnore
@@ -152,28 +133,4 @@ public class ProductController {
                 .body(new InputStreamResource(classPathResource.getInputStream()));
     }
 
-    public ContaDto validation(String token) throws Exception {
-        LinkedHashMap result = null;
-        if (token != null) {
-            if (token.contains("Basic")) {
-                return null;
-            }
-            HttpHeaders headers = new HttpHeaders();
-            RestTemplate restTemplate = new RestTemplate();
-            headers.setContentType(MediaType.APPLICATION_JSON);
-            headers.set("Authorization", "Bearer " + token);
-            HttpEntity<String> entity = new HttpEntity<String>(null, headers);
-            try {
-                result = restTemplate.postForObject("http://localhost:8080/validacao/token", entity, LinkedHashMap.class);
-            } catch (RestClientException e) {
-                e.printStackTrace();
-                return null;
-            }
-            ContaDto response = new ContaDto(result.get("hash").toString(), new BigDecimal(result.get("saldo").toString()), result.get("nome").toString(), result.get("email").toString(), result.get("cnpj").toString());
-
-
-            return response;
-
-        }
-        return null;
-}}
+}
